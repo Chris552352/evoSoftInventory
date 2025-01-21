a0fe29e65bd6e6c5c332a032f5ce4dab469f8682
Chris Manga <chris552352@gmail.com>
2025-01-21 16:44:56 +0100

    Création de la liste statique des magasins et produits

---
+++ src/data/products.ts
@@ -0,0 +1,14 @@
+import { Product } from "../types/Product";
+
+export const products: Product[] = [
+  { id: "PRO001", name: "Poulet", price: 5500 },
+  { id: "PRO002", name: "Spaghetti", price: 7000 },
+  { id: "PRO003", name: "Riz", price: 5000 },
+  { id: "PRO004", name: "Haricots Verts", price: 3000 },
+  { id: "PRO005", name: "Tomates", price: 2000 },
+  { id: "PRO006", name: "Oignons", price: 1500 },
+  { id: "PRO007", name: "Carottes", price: 2500 },
+  { id: "PRO008", name: "Piments", price: 1000 },
+  { id: "PRO009", name: "Huile de Cuisine", price: 8000 },
+  { id: "PRO010", name: "Sel", price: 500 },
+];
---
+++ src/data/stores.ts
@@ -0,0 +1,6 @@
+import { Store } from "../types/Store";
+
+export const stores: Store[] = [
+  { id: "MAG001", name: "Magasin A", address: "8025 BANDJOUN" },
+  { id: "MAG002", name: "Magasin B", address: "4590 BAGANGTE" },
+];
